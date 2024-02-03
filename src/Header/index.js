ec28b31bd148c6c6b0c014f76a9d18bb71b888ab
nicolas bertinat <nbertinatwork@gmail.com>
2024-02-03 12:27:31 -0300

    1 - Header ready

---
+++ src/Header/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./HeaderStyle.css";
+
+const Header = () => {
+  return (
+    <div>
+    <div className="header">
+      <img src="img/PiafLogo.png" alt="Piaf Logo" />
+
+<div className="headerUser">
+
+<img src="img/icon1.png" alt="Piaf Logo" />
+<img src="img/icon2.png" alt="Piaf Logo" />
+<img src="img/icon3.png" alt="Piaf Logo" />
+
+<div className="userImg">
+<img  src="img/user.png" alt="Piaf Logo" />
+</div>
+
+
+</div>
+</div>
+
+<div className="line"></div>
+
+    </div>
+  );
+}
+
+export default Header;
